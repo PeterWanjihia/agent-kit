876de368675deba73766e28f21c560380376ce57
ronexlemon <ronexondimu@gmail.com>
2026-05-19 12:02:46 +0300

    ft:agent identity details

--- pkg/chain/auth.go
+++ pkg/chain/auth.go
@@ -1,15 +1,14 @@
 package chain
 
-
 import (
 	"context"
-	
+
 	"math/big"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	
 )
 func MakeAuth(privateKeyHex string, chainID int64) (*bind.TransactOpts, error) {
 	privateKey, err := crypto.HexToECDSA(strings.TrimPrefix(privateKeyHex, "0x"))
@@ -27,4 +26,19 @@ func MakeAuth(privateKeyHex string, chainID int64) (*bind.TransactOpts, error) {
 
 	auth.Context = context.Background()
 	return auth, nil
+}
+
+func BindCallerOpts(ctx context.Context,address *common.Address)(*bind.CallOpts,error){
+
+	if ctx ==nil{
+		ctx = context.Background()
+	}
+	opts := &bind.CallOpts{
+		Context: ctx,
+	}
+	if address !=nil{
+		opts.From = *address
+	}
+	return opts,nil
+
 }
--- pkg/erc8004/client.go
+++ pkg/erc8004/client.go
@@ -2,6 +2,8 @@ package erc8004
 
 import (
 	"fmt"
+	"math/big"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -14,6 +16,16 @@ import (
 type ERC8004Client struct{
 	Registry *registry.Registry
 	Reputation *reputation.Reputation
+}
+type FeedbackInput struct{
+	AgentId *big.Int
+	Value *big.Int
+	ValueDecimals uint8
+    Tag1 string
+    Tag2 string
+    Endpoint string
+    FeedbackURI string
+    FeedbackHash [32]byte
 }
 
 func NewERC8004Client(eth *ethclient.Client,registryAddr string,reputationAddr string)(*ERC8004Client,error){
@@ -52,4 +64,64 @@ func (erc *ERC8004Client) RegisterAgent(auth *bind.TransactOpts,ipfsMetadata str
 
 	return tx,nil
 
+}
+
+func (erc *ERC8004Client) OwnerOfAgent(	opts *bind.CallOpts,agentID *big.Int) (common.Address, error) {
+
+	if opts == nil {
+		return common.Address{}, fmt.Errorf("call options are nil")
+	}
+
+	owner, err := erc.Registry.OwnerOf(opts, agentID)
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	return owner, nil
+}
+
+func (erc *ERC8004Client) AgentWallet(opts *bind.CallOpts,agent_id *big.Int)(common.Address,error){
+	address,err:=erc.Registry.GetAgentWallet(opts,agent_id)
+	if err !=nil{
+		return common.Address{},fmt.Errorf("Failed to get Agentic wallet")
+	}
+	return address,nil
+}
+
+
+func (erc *ERC8004Client) GetAgentMetadata(opts *bind.CallOpts,agent_id *big.Int,metadataKey string)([]byte,error){
+	data,err:=erc.Registry.GetMetadata(opts,agent_id,metadataKey)
+	if err !=nil{
+		return nil,fmt.Errorf("Failed to get metadata, reconfirm metadata key")
+	}
+	return  data,nil
+}
+
+
+func (erc *ERC8004Client) GetAgentTokenURI(
+	opts *bind.CallOpts,
+	agentID *big.Int,
+) (string, error) {
+
+	if opts == nil {
+		return "", fmt.Errorf("call opts is nil")
+	}
+
+	if agentID == nil {
+		return "", fmt.Errorf("agentID is required")
+	}
+
+	return erc.Registry.TokenURI(opts, agentID)
+}
+
+
+func (erc *ERC8004Client) AgentGiveFeedBack(opts *bind.TransactOpts,input FeedbackInput)(*types.Transaction,error){
+	return erc.Reputation.GiveFeedback(opts,input.AgentId,
+		input.Value,
+		input.ValueDecimals,
+		input.Tag1,
+		input.Tag2,
+		input.Endpoint,
+		input.FeedbackURI,
+		input.FeedbackHash)
 }
