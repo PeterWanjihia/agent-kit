406425d93f2c4b385d75db450e52d8999eb5dcd5
Peter Wanjihia <peterwanjihia@users.noreply.github.com>
2026-05-19 15:35:06 +0300

    feat(erc8004): add network-aware client resolver

--- pkg/chain/rpcs.go
+++ pkg/chain/rpcs.go
@@ -5,53 +5,53 @@ import "github.com/ronexlemon/agent-kit/pkg/types"
 var Mainnet_RPCS = map[string]types.NetworkConfig{
 	"Ethereum": {
 		ChainName: "ethereum",
-		RPC_URL:  "https://eth.llamarpc.com",
+		RPC_URL:   "https://eth.llamarpc.com",
 	},
 	"Base": {
 		ChainName: "base",
-		RPC_URL:  "https://mainnet.base.org",
+		RPC_URL:   "https://mainnet.base.org",
 	},
 	"Arbitrum": {
 		ChainName: "arbitrum",
-		RPC_URL:  "https://arb1.arbitrum.io/rpc",
+		RPC_URL:   "https://arb1.arbitrum.io/rpc",
 	},
 	"Avalanche": {
 		ChainName: "avalanche",
-		RPC_URL:  "https://api.avax.network/ext/bc/C/rpc",
+		RPC_URL:   "https://api.avax.network/ext/bc/C/rpc",
 	},
 	"Celo": {
 		ChainName: "celo",
-		RPC_URL:  "https://forno.celo.org",
+		RPC_URL:   "https://forno.celo.org",
 	},
 	"Monad": {
 		ChainName: "monad",
-		RPC_URL:  "https://rpc.monad.xyz",
+		RPC_URL:   "https://rpc.monad.xyz",
 	},
 }
 
 var Testnet_RPCS = map[string]types.NetworkConfig{
 	"Ethereum": {
 		ChainName: "ethereum",
-		RPC_URL:  "https://ethereum-sepolia-rpc.publicnode.com",
+		RPC_URL:   "https://ethereum-sepolia-rpc.publicnode.com",
 	},
 	"Base": {
 		ChainName: "base",
-		RPC_URL:  "https://sepolia.base.org",
+		RPC_URL:   "https://sepolia.base.org",
 	},
 	"Arbitrum": {
 		ChainName: "arbitrum",
-		RPC_URL:  "https://sepolia-rollup.arbitrum.io/rpc",
+		RPC_URL:   "https://sepolia-rollup.arbitrum.io/rpc",
 	},
 	"Avalanche": {
 		ChainName: "avalanche",
-		RPC_URL:  "https://api.avax-test.network/ext/bc/C/rpc",
+		RPC_URL:   "https://api.avax-test.network/ext/bc/C/rpc",
 	},
 	"Celo": {
 		ChainName: "celo",
-		RPC_URL:  "https://alfajores-forno.celo-testnet.org",
+		RPC_URL:   "https://forno.celo-sepolia.celo-testnet.org",
 	},
 	"Monad": {
 		ChainName: "monad",
-		RPC_URL:  "https://testnet-rpc.monad.xyz",
+		RPC_URL:   "https://testnet-rpc.monad.xyz",
 	},
 }
